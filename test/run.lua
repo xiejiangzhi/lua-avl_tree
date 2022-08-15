cc535b32bbaf1eaae6e4b664622a3fc1dde08d34
xiejiangzhi <xiejiangzhi@gmail.com>
2022-08-15 12:20:52 +0800

    Add more testing

--- test/run.lua
+++ test/run.lua
@@ -184,6 +184,14 @@ describe('rb_tree', function()
     r = {}
     ss:query(1.5, nil, -1, function(v) r[#r + 1] = v end)
     expect(r).to.equal({ 4, 3.5, 3, 2.5, 2 })
+
+    r = {}
+    ss:query(9.5, nil, -1, function(v) r[#r + 1] = v end)
+    expect(r).to.equal({ })
+
+    r = {}
+    ss:query(nil, -9, 1, function(v) r[#r + 1] = v end)
+    expect(r).to.equal({ })
   end)
 
   it('clear', function()
